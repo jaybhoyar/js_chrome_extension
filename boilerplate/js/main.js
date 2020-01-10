85af71e45ad70e66fc8b4b2cac033037a0735fcc
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-01-10 15:52:40 +0530

    initial setup

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,1 +1,10 @@
 // You js goes here
+function randomColor() {
+	let words = "ABCDEF0123456789".split("");
+	let color = "#";
+	for (i = 0; i < 6; i++) {
+		color += words[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+document.querySelector("body").style.backgroundColor = randomColor();
