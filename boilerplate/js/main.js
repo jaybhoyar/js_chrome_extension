57277aac217039a5b7380f9bec8de9f98e380448
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-01-18 13:16:56 +0530

    more icons add

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,43 +1,95 @@
 // You js goes here
 let textArea = document.querySelector(".text_input");
-let saveClip = document.querySelector(".save_clip_button");
+let search = document.querySelector(".search");
+let texttitle = document.querySelector(".text_input_title");
 let container = document.querySelector(".container_2");
-let clearAll = document.querySelector(".clearAll");
 let clips = JSON.parse(localStorage.getItem("clipArr")) || [];
 let id = Date.now();
 
 function createClips(event) {
-	if (event.keyCode == 13 && event.target.value.trim() != "") {
-		console.log(event.target.value);
+	if (
+		event.keyCode == 13 &&
+		event.target.value.trim() != "" &&
+		texttitle.value.trim() != ""
+	) {
+		console.log(event.target.value, texttitle.value);
 		const clip = {
-			name: event.target.value,
+			title: texttitle.value,
+			content: event.target.value,
 			id: ++id
 		};
 		clips.push(clip);
 		localStorage.setItem("clipArr", JSON.stringify(clips));
 		event.target.value = "";
+		texttitle.value = "";
 		clips = JSON.parse(localStorage.getItem("clipArr"));
 		viewClips(clips);
 	}
 }
 function viewClips(clipsArr) {
-	debugger;
 	container.innerHTML = "";
 	clipsArr.forEach(clip => {
-		let div = document.createElement("div");
-		div.setAttribute("data-id", clip.id);
-		div.classList.add("show_clip");
-		div.classList.add("show_clip_header");
-		div.innerText = clip.name;
-		container.append(p);
+		let parentDiv = document.createElement("div");
+		let titleDiv = document.createElement("div");
+		let contentDiv = document.createElement("div");
+		let titleP = document.createElement("p");
+		let spanX = document.createElement("span");
+		spanX.classList.add("spanX");
+		spanX.textContent = "X";
+
+		titleDiv.classList.add("title_styles");
+		contentDiv.classList.add("content_styles");
+		parentDiv.setAttribute("data-id", clip.id);
+		parentDiv.classList.add("show_clip", "show_clip_header");
+		titleP.innerText = clip.title;
+		contentDiv.innerText = clip.content;
+		titleDiv.append(titleP, spanX);
+		parentDiv.append(titleDiv, contentDiv);
+		container.append(parentDiv);
+		spanX.addEventListener("click", deleteClip);
+		contentDiv.addEventListener("click", selectValue);
 	});
 }
-function deleteClip() {
-	let target = event.target;
-	clips = clips.filter(clip => !(target.dataset.id == clip.id));
+function deleteClip(event) {
+	let target = event.target.parentElement;
+	// console.log(target.parentElement);
+	clips = clips.filter(clip => !(target.parentElement.dataset.id == clip.id));
 	localStorage.setItem("clipArr", JSON.stringify(clips));
-
 	viewClips(clips);
+}
+function selectValue(event) {
+	// console.log(event.target.innerHTML);
+	var range = document.createRange();
+	range.selectNode(event.target);
+	window.getSelection().removeAllRanges(); // clear current selection
+	window.getSelection().addRange(range); // to select text
+	document.execCommand("copy");
+	//window.getSelection().removeAllRanges(); // to deselect
+	setTimeout(addCopied, 0);
+	setTimeout(removeCopied, 2000);
+	function addCopied() {
+		event.target.innerText = "Copied!";
+		event.target.style.color = "#0A4DC8";
+		event.target.style.padding = "20px 0";
+		event.target.style.fontWeight = "600";
+		event.target.style.fontSize = "1.5rem";
+	}
+	function removeCopied() {
+		// event.target.innerText = text;
+		viewClips(clips);
+		// event.target.style.fontWeight = "400";
+		// event.target.style.fontSize = "1rem";
+		// event.target.style.color = "#000";
+	}
+}
+function searchClips(event) {
+	if (event.keyCode === 13 && event.target.value.trim() != "") {
+		let searchTitle = event.target.value;
+		let searched = clips.filter(clip => clip.title == searchTitle);
+		viewClips(searched);
+	} else {
+		viewClips(clips);
+	}
 }
 
 // function randomColor() {
@@ -49,33 +101,11 @@ function deleteClip() {
 // 	return color;
 // }
 viewClips(clips);
+
 // document.querySelector("body").style.backgroundColor = randomColor();
 // viewClips(clips);
 // textArea.addEventListener("keydown", createClips);
 textArea.addEventListener("keydown", createClips);
-clearAll.addEventListener("click", deleteAllClips);
+search.addEventListener("keydown", searchClips);
 //
 //  copy
-// function selectValue(event) {
-// 	var range = document.createRange();
-// 	range.selectNode(event.target);
-// 	window.getSelection().removeAllRanges(); // clear current selection
-// 	window.getSelection().addRange(range); // to select text
-// 	document.execCommand("copy");
-// 	//window.getSelection().removeAllRanges(); // to deselect
-// 	setTimeout(addCopied, 0);
-// 	setTimeout(removeCopied, 2000);
-// 	function addCopied() {
-// 		event.target.innerText = "Copied";
-// 		event.target.style.color = "#fff";
-// 		event.target.style.fontWeight = "600";
-// 		event.target.style.fontSize = "1.2rem";
-// 	}
-
-// 	function removeCopied() {
-// 		event.target.innerText = colors;
-// 		event.target.style.fontWeight = "400";
-// 		event.target.style.fontSize = "1rem";
-// 		event.target.style.color = "#000";
-// 	}
-// }
